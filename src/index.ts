554c9b9b77e0167ff43e1a46c2f8e6ab767bf250
Ziapa <SmiT4eggg@bk.ru>
2022-12-07 21:29:47 +0300

    add CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,73 @@
 import express, {Request, Response} from 'express'
+import bodyParser from "body-parser";
 
 const app = express()
 const PORT = 3000
 
+const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
+const addresses = [{id: 1, value: 'Germana'}, {id: 2, value: 'Veteranov'}]
+
+app.use(bodyParser({}))
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!!!!')
 })
+
+app.get('/products', (req: Request, res: Response) => {
+    if (req.query.title) {
+        let searchString = req.query.title.toString()
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+    } else {
+        res.send(products)
+    }
+})
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+    let product = products.find(p => p.title === req.params.productTitle)
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+
+app.delete('/products/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1)
+            res.send(204)
+            return
+        }
+    }
+    res.send(404)
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses)
+})
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    let address = addresses.find(p => p.id === +req.params.id)
+    if (address) {
+        res.send(address)
+    } else {
+        res.send(404)
+    }
+})
+
+app.post('/products', (req: Request, res: Response) => {
+    const newProducts = {id: +(new Date()), title: req.body.title}
+    products.push(newProducts)
+    res.status(201).send(newProducts)
+})
+app.put('/products/:id', (req: Request, res: Response) => {
+    let product = products.find(p => p.id === +req.params.id)
+    if (product) {
+        product.title = req.body.title
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on PORT ${PORT}`)
