d8363f083e627b63ebaea204d316b9090c2bd40e
Ziapa <SmiT4eggg@bk.ru>
2022-12-07 13:43:06 +0300

    vercel init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express'
 
 const app = express()
-const PORT = 3003
+const PORT = 3000
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!!!!')
